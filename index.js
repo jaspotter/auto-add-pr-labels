d20cfc00e00cbc7673a82118f7af73a2e1f288a5
Jason Potter <jaspotter@coca-cola.com>
2021-02-24 16:48:50 -0500

    updated logic and action.yml

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    const label = core.getInput('label');
-    console.log(`Got label: ${label}`);
-    const additionalLabels = core.getInput('additional-labels');
-    console.log(`Got additional labels: ${additionalLabels}`);
-    const payload = JSON.stringify(github.context.payload.pull_request, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    try {
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+        const octokit = github.getOctokit(GITHUB_TOKEN);
+        const { context = {} } = github;
+        const { pull_request } = context.payload;
+
+        const label = core.getInput('LABEL');
+        console.log(`Got label: ${label}`);
+        const additionalLabels = core.getInput('ADDITIONAL_LABELS').split(',');
+        console.log(`Got additional labels: ${additionalLabels}`);
+        const payload = JSON.stringify(github.context.payload.pull_request, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+        
+        await octokit.issues.addLabels({
+            owner: context.owner,
+            repo: context.repo,
+            issue_number: pull_request.prNumber,
+            labels: additionalLabels
+          });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+run();
